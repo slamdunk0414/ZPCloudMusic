67a02b7d048a04535647ec72f788a0cdcc09f376
yunlijingang <18600358960@163.com>
2020-05-28 15:04:39 +0800

    init

---
+++ CloudMusic/CloudMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudMusic
+//
+//  Created by 张鹏 on 2020/5/28.
+//  Copyright © 2020 张鹏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
