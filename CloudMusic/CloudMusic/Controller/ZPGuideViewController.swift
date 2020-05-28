45fc4791d9cd1cd05fa8bd0f7bdf7e31d79b0956
yunlijingang <18600358960@163.com>
2020-05-28 17:26:56 +0800

    第二页面等三秒后进入用户引导页面

--- CloudMusic/CloudMusic/Controller/ZPGuideViewController.swift
+++ CloudMusic/CloudMusic/Controller/ZPGuideViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ZPGuideViewController.swift
 //  CloudMusic
 //
 //  Created by 张鹏 on 2020/5/28.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ZPGuideViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .red
     }
 
 
-}
 
+}
---
+++ CloudMusic/CloudMusic/Controller/ZPSplashViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ZPSplashViewController.swift
+//  CloudMusic
+//
+//  Created by 张鹏 on 2020/5/28.
+//  Copyright © 2020 张鹏. All rights reserved.
+//
+
+import UIKit
+
+class ZPSplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+            self.next()
+        }
+    }
+    
+    func next(){
+        
+        let guide = ZPGuideViewController.init()
+
+        let appDelegate = AppDelegate.shared
+        appDelegate.window?.rootViewController = guide
+    }
+
+}
